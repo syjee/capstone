f486876587603365416531c6ab1fe13f645864c0
syjee <33935732+syjee@users.noreply.github.com>
2018-02-18 00:16:33 +0900

    Add files via upload

---
+++ TimeSync/test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TimeReception tr = new TimeReception();
+		tr.start();
+	}
+
+}
