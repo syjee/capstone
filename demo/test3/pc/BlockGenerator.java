2248466e37f7381000639b769fceb536fffe7f2a
syjee <33935732+syjee@users.noreply.github.com>
2018-03-27 02:33:13 +0900

    Add files via upload

---
+++ demo/test3/pc/BlockGenerator.java
@@ -0,0 +1,55 @@
+import java.security.NoSuchAlgorithmException;
+
+public class BlockGenerator extends Thread{
+	
+	public String previousHash;
+	public Block newBlock;
+	public Transaction request;
+	public int difficulty;
+	
+	public BlockchainManager bm; 
+	public boolean shouldRun = true;
+	public PeerNetworkManager pm;
+	public Blockchain bc;
+	
+	public BlockGenerator(Transaction request,PeerNetworkManager pm) {
+		// TODO Auto-generated constructor stub
+		this.request = request;
+		this.bm = new BlockchainManager();
+		this.pm = pm;
+		this.difficulty = request.getDifficulty();
+	}
+	
+	public BlockGenerator(Transaction request,PeerNetworkManager pm,Blockchain bc) {
+		// TODO Auto-generated constructor stub
+		this.request = request;
+		this.bm = new BlockchainManager();
+		this.pm = pm;
+		this.difficulty = request.getDifficulty();
+		this.bc = bc;
+	}
+	
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+	
+	public void run() {
+		while(shouldRun) {
+			try {
+				if(bm.getHeight() != 0)
+					newBlock = new Block(request.getTransaction(),bm.getRecentBlock().getCurrentHash());
+				else if(bm.getHeight() == 0)
+					newBlock = new Block(request.getTransaction());
+				
+				if(newBlock.mineBlock(difficulty)) {
+					bc.addToBlockQueue(newBlock);
+					pm.broadcast(newBlock);
+				}
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			shouldRun = false;
+		}
+	}
+}
