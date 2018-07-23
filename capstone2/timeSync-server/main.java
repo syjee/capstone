bd375f1d4405c7de2f7c84a1c9817577383e09cf
syjee <33935732+syjee@users.noreply.github.com>
2018-07-24 02:30:01 +0900

    Add files via upload

---
+++ capstone2/timeSync-server/main.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TimeReception tr = new TimeReception();
+		tr.setTimeServer(6);
+		
+		tr.start();
+
+	}
+	
+
+}
