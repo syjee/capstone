608390cf586dcd6bec98e00291434c6cf2e9d3dd
syjee <33935732+syjee@users.noreply.github.com>
2018-07-25 00:14:51 +0900

    Add files via upload

---
+++ 0725/192.168.11.10/TimeManagement.java
@@ -0,0 +1,36 @@
+import java.awt.RenderingHints.Key;
+import java.util.ArrayList;
+
+public class TimeManagement {
+
+	public KeyManager km;
+	public ArrayList<Time> mTime;
+
+	public TimeManagement(KeyManager km) {
+		this.km = km;
+		this.mTime = new ArrayList<Time>();
+	}
+
+	public void addTransaction(Transaction transaction, String sourceHost) {
+		String transactionHash = transaction.getTransaction();
+		String time = km.decrypt(transaction.getTransaction(), km.getPublicKey(sourceHost));
+
+		mTime.add(new Time(transactionHash, time, sourceHost));
+//		mTime.get(mTime.size() - 1).getStopWatch().start();
+	}
+
+	public void compare(Block block) {
+		String targetHash;
+		for (int k = 0; k < block.getArrayTransaction().size(); k++) {
+			targetHash = block.getArrayTransaction().get(k).getTransaction(); 
+			
+			for (int i = 0; i < mTime.size(); i++) {
+				if (mTime.get(i).getTransactionHash().equals(targetHash)) {
+					mTime.get(i).getStopWatch().pause();
+					mTime.remove(i);
+				}
+			}
+		}
+	}
+	
+}
