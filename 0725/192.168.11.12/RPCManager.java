94b412bd870a38ab31433090db57fb8412e38662
syjee <33935732+syjee@users.noreply.github.com>
2018-07-25 00:57:53 +0900

    Add files via upload

---
+++ 0725/192.168.11.12/RPCManager.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class RPCManager extends Thread{
+	public int term;
+	public boolean shouldRun = true;
+	public DBHelper db;
+	public RPC rpc;
+	public PeerNetworkManager pm;
+	public KeyManager km;
+	public Blockchain bc;
+	public BlockNetworkManager bnm;
+	
+	public RPCManager(PeerNetworkManager pm,Blockchain bc,KeyManager km,BlockNetworkManager bnm){
+		this.bc = bc;
+		this.pm = pm;
+		term = 30000;
+		db = new DBHelper();
+		this.km = km;
+		this.bnm = bnm;
+		rpc = new RPC(pm,bc,km,bnm);
+		
+		
+	}
+	
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+		//	e1.printStackTrace();
+		}
+		// TODO Auto-generated method stub
+		rpc.start();
+		while(shouldRun){
+			Scanner scan;
+			String host="";
+			
+			try {				
+				scan = new Scanner(db.getPeerListFile());
+				while (scan.hasNextLine()) 
+		        { 
+		            host = scan.nextLine(); 
+		            if(!db.integrityCheck(db.getRPCListFile(), host))
+		            	rpc.connectToPeer(host, 8016);
+		        }
+				scan.close(); 
+				Thread.sleep(term);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("connectToPeer ( "+host+" ) detected an error...");
+			}
+		}
+	}
+	
+	public void request(){
+		rpc.request();
+    }
+	
+	public void request(int difficulty) throws Exception{
+		rpc.request(difficulty);
+    }
+	
+	public void request(Block newBlock){
+		rpc.request(newBlock);
+	}
+	
+	public void request(int difficulty,Block newBlock){
+		newBlock.setDifficulty(difficulty);
+		rpc.request(newBlock);
+	}
+}
